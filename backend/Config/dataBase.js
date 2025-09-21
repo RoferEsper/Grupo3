f60fe2af483d3da4883ac046a3e5d40d0cea2286
miguelbarilaro <barilaromiguel@gmail.com>
2025-09-21 19:30:09 -0300

    Documentacion

--- backend/Config/dataBase.js
+++ backend/Config/dataBase.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '170800',
-    database: 'db_inst'
+    password: 'Bari',
+    database: 'db_inst' 
 });
 
 connection.connect((err) => {
